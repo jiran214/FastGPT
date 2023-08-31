608d0a424cf514face4ec5c8456f8f6fae4c403c
jiran214 <jiran214@qq.com>
2023-09-01 01:05:17 +0800

    update

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -75,7 +75,7 @@ const Login = () => {
       >
         {isPc && (
           <Image
-            src={'/icon/loginLeft.svg'}
+            src={'/icon/loginLeft.png'}
             order={pageType === PageTypeEnum.login ? 0 : 2}
             flex={'1 0 0'}
             w="0"
